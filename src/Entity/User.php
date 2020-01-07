868a5e1ac3a544f19398ea85d23299c85252f6eb
Cedric Pierrard <cdricmp78@gmail.com>
2020-01-07 17:36:01 +0100

    Accueil + FosUser

---
+++ src/Entity/User.php
@@ -0,0 +1,104 @@
+<?php
+// src/Entity/User.php
+
+namespace App\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ * @UniqueEntity(fields={"username"}, message="There is already an account with this username")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $Nom;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $Prenom;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $ville;
+
+    /**
+     * @ORM\Column(type="integer", length=10)
+     */
+    private $numero;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->Prenom;
+    }
+
+    public function setPrenom(string $Prenom): self
+    {
+        $this->Prenom = $Prenom;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getNumero(): ?string
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(string $numero): self
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+}
